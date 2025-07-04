4c19f4dca162a7f6fc17e473718fe6acdeb2fdfe
bekizod <bekizodcancer@gmail.com>
2025-07-04 14:26:26 -0700

    adding fastify guard

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationFilter } from './filters/validation.filter';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   app.useGlobalFilters(new ValidationFilter());
   app.enableCors({
     origin: [
